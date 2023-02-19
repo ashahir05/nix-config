84ddf4e6e5d1d560a8703ad0d6d300a207395ae1
Ahmed Shahir Samin <ashahir05@gmail.com>
2023-02-19 20:38:32 +0600

    Automated commit on Sun 19 Feb 20:38:32 +06 2023

--- home-manager/neovim.nix
+++ home-manager/neovim.nix
@@ -1,6 +1,6 @@
 { inputs, outputs, lib, config, pkgs }: {
   programs.neovim.enable = true;
-  programs.neovim.plugins = [
-    (pkgs.nvim-treesitter.withPlugins (_: pkgs.tree-sitter.allGrammars))
+  programs.neovim.plugins = with pkgs; [
+    (nvim-treesitter.withPlugins (_: pkgs.tree-sitter.allGrammars))
   ];
 }
