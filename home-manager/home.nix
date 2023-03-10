d225dd9c7cddd1890335541b273e699b2ea12219
Ahmed Shahir Samin <ashahir05@gmail.com>
2023-03-10 10:42:44 +0600

    Automated commit on Fri 10 Mar 10:42:44 +06 2023

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -26,6 +26,8 @@
   programs.git.userName = "Ahmed Shahir Samin";
   programs.git.userEmail = "ashahir05@gmail.com";
 
+  programs.bash.enable = true;
+
   gtk.enable = true;
   gtk.theme.name = "Adw-gtk3";
   gtk.theme.package = pkgs.adw-gtk3;
