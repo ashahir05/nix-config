69485ee96d4af23252c13606daa76ea629300f12
Ahmed Shahir Samin <ashahir05@gmail.com>
2023-07-25 22:55:53 +0600

    Automated commit on Tue 25 Jul 22:55:53 +06 2023

--- packages/pollymc/default.nix
+++ packages/pollymc/default.nix
@@ -1,83 +1,35 @@
 { 
+  buildFHSEnv ? (import <nixpkgs> {}).buildFHSEnv,
   stdenv ? (import <nixpkgs> {}).stdenv,
   lib ? (import <nixpkgs> {}).lib,
   pkgs ? (import <nixpkgs> {}),
   localPkgs
-}:
-  let
-    pname = "pollymc";
-    name = "PollyMC-${version}";
-    version = "7.1";
-
-    src = builtins.fetchurl {
-      url = "https://github.com/fn2006/PollyMC/releases/download/${version}/PollyMC-Linux-${version}.tar.gz";
-      sha256 = "sha256:1wazsg0r8l0mqbzdcxp0iv322ll4yk2cv29wwm6v1cgmq7kp90bl";
-    };
-  in
-    stdenv.mkDerivation rec {
-      inherit pname name src;
-      
-      nativeBuildInputs = with pkgs; [
-        autoPatchelfHook
-        makeBinaryWrapper
-        qt5.wrapQtAppsHook
-      ];
-      
-      xorgPkgs = with pkgs.xorg; [
-        libXext
-        libX11
-        libXext
-        libXcursor
-        libXrandr
-        libXxf86vm
-        libXrender
-        libXtst
-        libXi
-      ];
-      
-      buildInputs = with pkgs; [
-        qt5.qtbase
-        libGL
-        localPkgs.glfw
-        openal
-        flite
-        freetype
-        fontconfig
-      ] ++ xorgPkgs;
-      
-      dontWrapQtApp = true;
-      
-      libPath = lib.makeLibraryPath(with pkgs; [
-        libGL
-        localPkgs.glfw
-        openal
-        flite
-        freetype
-        fontconfig
-      ] ++ xorgPkgs);
-      
-      unpackPhase = ''
-        tar -xzf $src
-      '';
-      
-      installPhase =  ''
-        mkdir $out
-        cp -r ./* $out
-        
-        runHook postInstall
-      '';
-      
-      postInstall = ''
-        mv $out/bin/pollymc $out/bin/pollymc-unwrapped
-        makeWrapper $out/bin/pollymc-unwrapped $out/bin/pollymc \
-          --set LD_LIBRARY_PATH /run/opengl-driver/lib:${libPath} \
-          --set OPENGL ${pkgs.libGL}/lib \
-          --set GLFW ${localPkgs.glfw}/lib \
-          --set OPENAL ${pkgs.openal}/lib
-      '';
-      
-      pathsToLink = [
-        "/share"
-        "/bin"
-      ];
-    }
+}: 
+let
+  pollymc = pkgs.callPackage ./pollymc.nix { inherit stdenv lib pkgs localPkgs; }
+in
+  buildFHSEnv {
+    name = "pollymc";
+    
+    targetPkgs = pkgs: (with pkgs; [
+      qt5.qtbase
+      freetype
+      fontconfig
+    ]) ++ (with pkgs.xorg; [
+      libXext
+      libX11
+      libXext
+      libXcursor
+      libXrandr
+      libXxf86vm
+      libXrender
+      libXtst
+      libXi
+    ]);
+    
+    extraInstallCommands = ''
+      ln -s "${pollymc}/share" "$out/"
+    '';
+    
+    runScript = "${pollymc}/bin/pollymc";
+  }
