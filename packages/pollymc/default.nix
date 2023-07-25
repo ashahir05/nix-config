ac0ec572500ef938029197f59de0a674dc718a1b
Ahmed Shahir Samin <ashahir05@gmail.com>
2023-07-25 15:43:28 +0600

    Automated commit on Tue 25 Jul 15:43:28 +06 2023

--- packages/pollymc/default.nix
+++ packages/pollymc/default.nix
@@ -29,7 +29,7 @@
         localPkgs.glfw
         openal
         flite
-        libXext
+        xorg.libXext
       ];
       
       dontWrapQtApp = true;
@@ -39,7 +39,7 @@
         localPkgs.glfw
         openal
         flite
-        libXext
+        xorg.libXext
       ]);
       
       unpackPhase = ''
