2b10ec73cdc65484bd608f3ba7029f05a57245fc
Ahmed Shahir Samin <ashahir05@gmail.com>
2023-07-25 13:53:51 +0600

    Automated commit on Tue 25 Jul 13:53:51 +06 2023

--- packages/pollymc/default.nix
+++ packages/pollymc/default.nix
@@ -53,9 +53,9 @@
         mv $out/bin/pollymc $out/bin/pollymc-unwrapped
         makeWrapper $out/bin/pollymc-unwrapped $out/bin/pollymc \
           --set LD_LIBRARY_PATH /run/opengl-driver/lib:${libPath} \
-          --set OPENGL ${pkgs.libGL} \
-          --set GLFW ${localPkgs.glfw} \
-          --set OPENAL ${pkgs.openal}
+          --set OPENGL ${pkgs.libGL}/lib \
+          --set GLFW ${localPkgs.glfw}/lib \
+          --set OPENAL ${pkgs.openal}/lib
       '';
       
       pathsToLink = [
